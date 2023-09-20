c55b4d1bd47b81710fd1a892be37b30abda49223
jhoan.polania <sebastian.polania.dev@gmail.com>
2023-09-20 08:35:26 -0500

    Proyecto iniciado

---
+++ domain/droneapp.domain/src/main/java/org/jsapps/droneapp/domain/gateway/ICreateDroneDAO.java
@@ -0,0 +1,4 @@
+package org.jsapps.droneapp.domain.gateway;
+
+public interface ICreateDroneDAO {
+}
---
+++ infrastructure/droneapp.api/src/main/java/org/jsapps/droneapp/api/DroneController.java
@@ -0,0 +1,9 @@
+package org.jsapps.droneapp.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/drone")
+public class DroneController {
+}
